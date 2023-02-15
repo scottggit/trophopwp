3e7bbf5161cdae5b2c97b021af7c7c01946bdab6
Ranendra <ranendramaharjan@gmail.com>
2023-02-15 16:15:05 +0545

    Representatives Updates

--- page-representatives.php
+++ page-representatives.php
@@ -1,4 +1,4 @@
-<?php get_header();?>
+<?php get_header(); ?>
 <style>
     @media screen and (min-width: 768px) {
         .bg-title-white {
@@ -28,35 +28,35 @@
     }
 </style>
 <div class="content">
-      <section class="page-banner mb-5 pb-5">
-      <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-6 col-md-6 p-0 offset-lg-1 order-md-2 mb-4 mb-md-0">
+    <section class="page-banner pb-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 p-0 offset-lg-1 order-md-2 mb-4 mb-md-0">
                     <img src="<?php bloginfo('template_directory') ?>/dist/img/representative-banner.png" alt="" class="w-100 rep-banner">
                 </div>
                 <div class="col-lg-4 col-md-6 offset-lg-1 order-md-1">
                     <div class="d-flex align-items-center h-100">
                         <div class="searchwrap stores-search">
-                            <h1 class="display-1 mb-4"><span class="bg-title-white">Distribution</span></h1>
-                            <?PHP  echo do_shortcode('[searchandfilter id="492"]'); ?>
-                         
+                            <h1 class="display-1 mb-4"><span class="bg-title-light position-relative pe-lg-2 z-2">Distribution</span></h1>
+                            <?PHP echo do_shortcode('[searchandfilter id="492"]'); ?>
+
                         </div>
                     </div>
                 </div>
-      </div>
-      </div>
-      </section>
+            </div>
+        </div>
+    </section>
 
-      <section class="stores " id="stores">
+    <section class="stores " id="stores">
         <div class="container">
-        <?PHP  echo do_shortcode('[searchandfilter id="492" show="results"]'); ?>  
+            <?PHP echo do_shortcode('[searchandfilter id="492" show="results"]'); ?>
 
-      </section>
+    </section>
 </div>
-<?php get_footer();?>
+<?php get_footer(); ?>
 <script>
-$('.previouspostslink, .page, .last, .nextpostslink').each( function(i,a) {
-         $(a).attr('href',$(a).attr('href')+'#stores');
-   
-     });
+    $('.previouspostslink, .page, .last, .nextpostslink').each(function(i, a) {
+        $(a).attr('href', $(a).attr('href') + '#stores');
+
+    });
 </script>
--- search-filter/492.php
+++ search-filter/492.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Search & Filter Pro 
  *
@@ -22,79 +23,79 @@
  *
  */
 
-$count=0;
-if ( $query->have_posts() ){
+$count = 0;
+if ($query->have_posts()) {
 	$total_posts = $query->found_posts;
-	?>
+?>
 	<?php
 
-$current_page = $query->query_vars['paged'];
-$current_post_count = $query->post_count - 1;
-$posts_per_page = $query->query_vars['posts_per_page'];
-$total_posts = $query->found_posts;
+	$current_page = $query->query_vars['paged'];
+	$current_post_count = $query->post_count - 1;
+	$posts_per_page = $query->query_vars['posts_per_page'];
+	$total_posts = $query->found_posts;
 
-$posts_start = ($current_page - 1 ) * $posts_per_page + 1;
-if( $posts_per_page > $total_posts ) {
-	$posts_start = 1;
-}
-$posts_end = $posts_start + $current_post_count;
+	$posts_start = ($current_page - 1) * $posts_per_page + 1;
+	if ($posts_per_page > $total_posts) {
+		$posts_start = 1;
+	}
+	$posts_end = $posts_start + $current_post_count;
 
-?>
+	?>
 
-	<div class="row ">
+	<div class="row mb-lg-5 pb-5">
 		<?php
-			while ($query->have_posts()){
+		while ($query->have_posts()) {
 			$query->the_post();
 		?>
-	 
-<div class="col-lg-3 col-md-6">  
-<div class="strore-wrap">
 
-          <div class="logo">
-            <?php if(get_field('logo')){ ?>
-      <img src="<?php  echo get_field('logo'); ?>" alt="">
-      <?php }else{ ?>
-        <img src="<?php bloginfo('template_directory')?>/dist/img/logo_placeholder.png" alt="">
-    <?php } ?>
-          </div>
-<div class="desc-wrap">
-      <h3 class="name"><?php echo get_the_title(); ?></h3>
-      <?php if(get_field('address')){ ?>
-      <div class="location"><img src="<?php bloginfo('template_directory')?>/dist/img/location.png" alt=""> <?php echo get_field('town');?>, <?php echo get_field('address');?></div>
-      <?php } ?>
-      <?php if(get_field('phone')){ ?>
+			<div class="col-xl-3 col-lg-4 col-md-6 store-spacing">
+				<div class="strore-wrap bg-white h-100">
+					<div class="logo">
+						<?php if (get_field('logo')) { ?>
+							<img src="<?php echo get_field('logo'); ?>" alt="">
+						<?php } else { ?>
+							<img src="<?php bloginfo('template_directory') ?>/dist/img/logo_placeholder.png" alt="">
+						<?php } ?>
+					</div>
+					<div class="desc-wrap">
+						<p class="name text-dark font-bold mb-0"><?php echo get_the_title(); ?></p>
+						<?php if (get_field('address')) { ?>
+							<small class="d-flex align-items-start location"><img src="<?php bloginfo('template_directory') ?>/dist/img/location.png" alt=""> <?php echo get_field('town'); ?>, <?php echo get_field('address'); ?></small>
+						<?php } ?>
+						<?php if (get_field('phone')) { ?>
 
-      <div class="phone"><img src="<?php bloginfo('template_directory')?>/dist/img/phone.png" alt=""><?php echo get_field('phone');?></div>
-      <?php } ?>
-      <?php if(get_field('email')){ ?>
-      <div class="email"><img src="<?php bloginfo('template_directory')?>/dist/img/email.png" alt=""><?php echo get_field('email');?></div>
-      <?php } ?>
-      <?php if(get_field('time')){ ?>
-      <div class="clock"><img src="<?php bloginfo('template_directory')?>/dist/img/clock.png" alt=""><?php echo get_field('time');?></div>
-      <?php } ?>
-      </div>
+							<small class="d-flex align-items-start phone"><img src="<?php bloginfo('template_directory') ?>/dist/img/phone.png" alt=""><?php echo get_field('phone'); ?></small>
+						<?php } ?>
+						<?php if (get_field('email')) { ?>
+							<small class="d-flex align-items-start email"><img src="<?php bloginfo('template_directory') ?>/dist/img/email-primary.svg" alt=""><?php echo get_field('email'); ?></small>
+						<?php } ?>
+						<?php if (get_field('time')) { ?>
+							<small class="d-flex align-items-start clock"><img src="<?php bloginfo('template_directory') ?>/dist/img/clock.png" alt=""><?php echo get_field('time'); ?></small>
+						<?php } ?>
+					</div>
 
-      <div class="btn-wrap">
-            <a href="https://www.google.com/maps?q=<?php echo get_field('latitude'); ?>,<?php echo get_field('longitude'); ?>" class="directions">Directions</a>
-            <a href="<?php echo get_field('website');?>" class="website">Website</a>
-      </div>
-      </div>
-</div>
-		<?php  $count++; ?>
+					<div class="btn-wrap d-flex flex-wrap gap-2">
+						<a href="https://www.google.com/maps?q=<?php echo get_field('latitude'); ?>,<?php echo get_field('longitude'); ?>" class="directions" target="_blan">Directions</a>
+						<a href="<?php echo get_field('website'); ?>" class="website shadow-sm" target="_blank">Website</a>
+					</div>
+				</div>
+			</div>
+			<?php $count++; ?>
 		<?php
-			}
+		}
 		?>
+	</div>
 	<?php // Pagination
 	$total = $query->max_num_pages;
 
 	// only bother with pagination if we have more than 1 page
-	if ( $total > 1 ) : ?>
+	if ($total > 1) : ?>
 		<div class="wp-pagenavi">
 			<?php
 			// Set up pagination to use later on
 			$big = 999999999; // need an unlikely integer
-			$pagination = paginate_links( array(
-				'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+			$pagination = paginate_links(array(
+				'base'      => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
 				'format'    => '?paged=%#%',
 				'current'   => $current_page,
 				'total'     => $total,
@@ -102,17 +103,14 @@ $posts_end = $posts_start + $current_post_count;
 				'prev_next' => false,
 				'prev_text' => __('', 'visceral'),
 				'next_text' => __('', 'visceral')
-			) );
+			));
 
 			echo $pagination; ?>
 		</div>
 	<?php endif; ?>
-	</div>
 
-	<?php
-}
-else
-{
+<?php
+} else {
 	echo "No Results Found";
 }
 ?>
